15c67cb6c8f05c7ae64008f301d479071accfece
Thio Saputra <132740827+flessan@users.noreply.github.com>
2026-09-23 21:08:15 +0800

    Add admin configuration and badge foundations

---
+++ public_html/config/badges.php
@@ -0,0 +1,16 @@
+<?php
+
+$roleBadges = [
+    1 => [
+        'name' => 'Moderator',
+        'image' => 'https://raw.githubusercontent.com/Fenix668/GMDprivateServer/master/dashboard/modBadge_01_001.png'
+    ],
+    2 => [
+        'name' => 'Senior Moderator',
+        'image' => 'https://raw.githubusercontent.com/Fenix668/GMDprivateServer/master/dashboard/modBadge_02_001.png'
+    ],
+    3 => [
+        'name' => 'Administrator',
+        'image' => 'https://raw.githubusercontent.com/Fenix668/GMDprivateServer/master/dashboard/modBadge_03_001.png'
+    ],
+];
